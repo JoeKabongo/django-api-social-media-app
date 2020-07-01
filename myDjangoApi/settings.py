c851e4a3af2670b3dbfd72e7d30ed23f5f32daa3
JoeKabongo <jonathankabongo23@gmail.com>
2020-06-30 22:25:56 -0700

    adding file to gitginore

--- myDjangoApi/settings.py
+++ myDjangoApi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hmoh*bpl3py=^+fb+*0vk6(^5n@37y!$t#oo0c!ol0wz33pzq6'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
